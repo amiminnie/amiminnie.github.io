8fc75e85905786ed20a406f47d4ddbd242e47770
amiminnie <minushadulwan@gmail.com>
2026-07-21 09:02:02 +0530

    added updater workflow, now used for duolingo stats

---
+++ scripts/updater.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+USERNAME = "amiminnie"
+
+url = f"https://www.duolingo.com/2017-06-30/users?username={USERNAME}"
+
+r = requests.get(url)
+r.raise_for_status()
+
+user = r.json()["users"][0]
+
+output = {
+    "username": user["username"],
+    "xp": user.get("totalXp"),
+    "streak": user.get("streak"),
+    "courses": []
+}
+
+for course in user.get("courses", []):
+    output["courses"].append({
+        "language": course.get("title"),
+        "xp": course.get("xp"),
+        "level": course.get("level")
+    })
+
+with open("duodata.json", "w") as f:
+    json.dump(output, f, indent=4)
